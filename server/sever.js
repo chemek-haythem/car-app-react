b34c2ca7e9e1907feb8ac14eab813a39a114a4e1
haythem <haythemchemek2003@gmail.com>
2024-11-22 11:53:27 +1300

    first commit

---
+++ server/sever.js
@@ -0,0 +1,65 @@
+
+import express from 'express';
+import mysql from 'mysql';
+import cors from 'cors';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+app.use(express.json());
+app.use(cors({
+    origin: ["http://localhost:3000"],
+    methods: ["POST", "GET"],
+    credentials: true
+}));
+app.use(cookieParser());
+
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: "",
+    database: 'stage'
+});
+
+
+db.connect(err => {
+    if (err) {
+        console.error('Error connecting to the database:', err);
+        return;
+    }
+    console.log('Connected to the database.');
+});
+
+app.post('src/modules/Login', (req, res) => {
+    const sql = 'SELECT * FROM login WHERE email = ?';
+    db.query(sql, [req.body.email], (err, data) => {
+        if (err) return res.json({ Error: "Login error in server" });
+        if (data.length > 0) {
+            bcrypt.compare(req.body.password.toString(), data[0].password, (err, response) => {
+                if (err) return res.json({ Error: "Password compare error" });
+                if (response) {
+                    const name = data[0].name;
+                    const token = jwt.sign({ name }, "jwt-secret-key", { expiresIn: '1d' });
+                    res.cookie('token', token);
+                    return res.json({ Status: "Success" });
+                } else {
+                    return res.json({ Error: "Password not matched" });
+                }
+            });
+        } else {
+            return res.json({ Error: "No email existed" });
+        }
+    });
+});
+
+app.use((err, req, res, next) => {
+    console.error('Unhandled error:', err);
+    res.status(500).json({ Error: "Internal Server Error" });
+});
+
+
+app.listen(8080, () => {
+    console.log('Server running on port 8080...');
+});
